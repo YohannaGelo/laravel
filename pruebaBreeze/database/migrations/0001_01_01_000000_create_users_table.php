8aa5265dafec037d43ce46d6206319618d9d23aa
YohannaGelo <yohannagelo@gmailcom>
2025-02-25 10:49:01 +0100

    prueba tabla

--- pruebaBreeze/database/migrations/0001_01_01_000000_create_users_table.php
+++ pruebaBreeze/database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,18 +7,26 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations. Crear Migraciones
      */
     public function up(): void
     {
+        // Aqui users es el nombre de la tabla que queremos
+        // Crea un objeto blueprint y así creamos las columnas de la tabla
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id();   // int autoincrement
+            // Nombre de las columnas van entre 'nombre'
+            $table->string('name', 100); // varchar (255 por defecto))
+            // $table->text('texto'); // varchar para mas de 255
+            $table->string('email')->unique();  // mail único 
+
+            // Se puede dejar vacio (nullable) sino es obligatorio
+            $table->timestamp('email_verified_at')->nullable(); // guarda fechas para verificar por ejemplo un mail, en la fecha que se verifica
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();  // varchar 100, almacena un token para mantener sesion iniciada
+ 
+            $table->timestamps();   // 2 columnas de timestamp -> 1 fecha de cuando se ha insertado y 2 fecha de la ultima modificación
+                                    //created_at - updated_up
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -38,10 +46,11 @@ return new class extends Migration
     }
 
     /**
-     * Reverse the migrations.
+     * Reverse the migrations. Sirve para deshacer la migración, borra la tabla
      */
     public function down(): void
     {
+        //Podríamos añadir aquí alguna lógica necesaria para que ocurra al deshacer la migración
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
---
+++ pruebaBreeze/database/migrations/2025_02_25_090115_create_players_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+        
+            $table->string('name', 150);
+            $table->text('description', 400)->nullable;
+
+            $table->timestamp('date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
